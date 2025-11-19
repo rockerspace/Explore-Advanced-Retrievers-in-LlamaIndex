fe93e94c0df99ec04db4f368567c1a57e4d3fa72
Narendra Ragav Venkatesan <147092133+rockerspace@users.noreply.github.com>
2025-11-19 13:29:01 +0530

    Add pip install command for required packages

---
+++ mycode.py
@@ -0,0 +1,8 @@
+!pip install llama-index==0.12.49 \
+    llama-index-embeddings-huggingface==0.5.5 \
+    llama-index-llms-ibm==0.4.0 \
+    llama-index-retrievers-bm25==0.5.2 \
+    sentence-transformers==5.0.0 \
+    rank-bm25==0.2.2 \
+    PyStemmer==2.2.0.3 \
+    ibm-watsonx-ai==1.3.31 | tail -n 1
